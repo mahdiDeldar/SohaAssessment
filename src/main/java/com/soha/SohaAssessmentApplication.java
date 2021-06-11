3b17dfe1b630bda3ea87508b9b9acbbe2c47c9f8
mahdi <deldarm@ymail.com>
2021-06-11 13:46:14 +0330

    first commit

---
+++ src/main/java/com/soha/SohaAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.soha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SohaAssessmentApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SohaAssessmentApplication.class, args);
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/soha/SohaAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SohaAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
